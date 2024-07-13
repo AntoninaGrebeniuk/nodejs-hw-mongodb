5e3f161bb2291997c0345f9344954c9e05dd9747
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-07-13 19:09:53 +0400

    remove commits

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-// src/server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-// import dotenv from 'dotenv';
+
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
-
-// dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
 
