bc8f406125c870a650d9cdb104a29200cf734a5c
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-08-09 23:20:27 +0400

    add reset password logic

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,6 @@
+import path from 'node:path';
+import { env } from '../utils/env.js';
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -5,3 +8,13 @@ export const SORT_ORDER = {
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 30 * 24 * 60 * 60 * 1000;
+
+export const SMTP = {
+  SMTP_HOST: env('SMTP_HOST'),
+  SMTP_PORT: env('SMTP_PORT'),
+  SMTP_USER: env('SMTP_USER'),
+  SMTP_PASSWORD: env('SMTP_PASSWORD'),
+  SMTP_FROM: env('SMTP_FROM'),
+};
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
